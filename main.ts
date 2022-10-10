26e3c953ae397ed69748f49925112050490560a7
yhysophie <115450383+yhysophie@users.noreply.github.com>
2022-10-10 14:51:18 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(1)
+    basic.showNumber(2)
+    basic.showNumber(3)
+    basic.showNumber(4)
+    basic.showNumber(5)
+    basic.showNumber(6)
+})
+basic.forever(function () {
+	
+})
